24fb3986d7496cfa70d05a055539306c08a8df25
Josh Pham <joshpham.concordia@gmail.com>
2024-10-15 18:21:36 -0400

    Test DB host connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 chmod +x /app/wait-for-it.sh
 
-echo ${POSTGRES_HOST}
+echo "Postgres host is ${POSTGRES_HOST}"
 
-/bin/bash /app/wait-for-it.sh db:5432 -t 300 -h ${POSTGRES_HOST} -- bash startup.sh
+/bin/bash /app/wait-for-it.sh db:5432 -t 300 -h ep-lively-shape-a4xbenvr-pooler.us-east-1.aws.neon.tech -- bash startup.sh
