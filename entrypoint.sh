6a176746873633162c1144f889c39f13322690f8
Josh Pham <joshpham.concordia@gmail.com>
2024-10-22 22:41:01 -0400

    Fix build args not a list

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 chmod +x /app/wait-for-it.sh
 
-echo "Postgres host is ${POSTGRES_HOST}"
+echo "Postgres host is ${POSTGRES_HOST}:${POSTGRES_PORT}"
 
-/bin/bash /app/wait-for-it.sh db:5432 -t 300 -h ep-lively-shape-a4xbenvr-pooler.us-east-1.aws.neon.tech -- bash startup.sh
+/bin/bash /app/wait-for-it.sh db:${POSTGRES_PORT} -t 300 -h ${POSTGRES_HOST} -- bash startup.sh
